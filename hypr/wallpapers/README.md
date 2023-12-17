26a94ae2409bb63dd7bcb33548a3652a6991df28
42willow <70784752+42Willow@users.noreply.github.com>
2023-12-17 19:21:19 +1100

    Customise populate.py

---
+++ hypr/wallpapers/README.md
@@ -0,0 +1,33 @@
+
+<!-- HEADERS -->
+<p align="center">
+  <img width="25%" src="https://github.com/42Willow/dotfiles/blob/main/assets/42willow.gif?raw=true" />
+</p>
+<p align="center">
+  <b> ~ Willow's wallpaper dump ~ </b>
+</p>
+
+All wallpapers here are suitable for a 4K monitor :)
+Inspired by [flick0](https://github.com/flick0/kabegami)
+
+-----------------
+
+## Art
+<details><summary></summary>
+<img src="./Art/willow_and_sundew.jpeg" title="willow_and_sundew."><br>
+</details>
+
+## Photos
+<details><summary></summary>
+<img src="./Photos/sandstone.jpg" title="sandstone"><br>
+<img src="./Photos/mountains.jpg" title="mountains"><br>
+<img src="./Photos/lake.jpg" title="lake"><br>
+</details>
+
+## Pixel Art
+<details><summary></summary>
+<img src="./Pixel Art/1.gif" title="1"><br>
+<img src="./Pixel Art/2.gif" title="2"><br>
+<img src="./Pixel Art/3.gif" title="3"><br>
+<img src="./Pixel Art/4.png" title="4"><br>
+</details>
--- hypr/wallpapers/populate.py
+++ hypr/wallpapers/populate.py
@@ -1,35 +1,53 @@
-from email.mime import image
 import os
 
-image_folders = ["art", "abstract", "irl", "minimal", "tiles","animated"]
+# # Get folders in the current working directory
+# image_folders = [folder for folder in os.listdir() if os.path.isdir(os.path.join(os.getcwd(), folder))]
+# image_folders.sort() # Sort alphabetically
+# print(image_folders)
+
+base_directory = "/home/willow/.dotfiles/hypr/wallpapers/"
+readme = base_directory + "README.md"
+
+# Discover image folders in the base directory
+image_folders = [folder for folder in os.listdir(base_directory) if os.path.isdir(os.path.join(base_directory, folder))]
 image_folders.sort()
-print(image_folders)
+# print(image_folders)
 
-pre = """
-# welcome to my personal wallpaper dump
-
-## previews
-<hr>
+# Begin README.md
+pre = f"""
+<!-- HEADERS -->
 <p align="center">
+  <img width="25%" src="https://github.com/42Willow/dotfiles/blob/main/assets/42willow.gif?raw=true" />
+</p>
+<p align="center">
+  <b> ~ Willow's wallpaper dump ~ </b>
+</p>
+
+All wallpapers here are suitable for a 4K monitor :)
+Inspired by [flick0](https://github.com/flick0/kabegami)
+
+-----------------
 """
 
-post = """
-</p>
-"""
-with open("./README.md", "w") as f:
+# End README.md
+post = ""
+
+with open(readme, "w") as f:
     f.write(pre)
 
 def image_embed(title,folder,img):
     return f"""<img src="./{folder}/{img}" title="{title}"><br>\n"""
 
-with open("./readme.md", "a") as readme:
+with open(readme, "a") as f:
     for folder in image_folders:
-        readme.write("\n\n## " + folder + "\n")
-        readme.write("<details><summary></summary>\n")
-        for file in os.listdir(folder):
-            if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".gif") or file.endswith(".webp") or file.endswith(".webm"):
-                readme.write(
+        f.write("\n## " + folder + "\n")
+        f.write("<details><summary></summary>\n")
+        print(f"=== Folder: {folder} ===")
+        for file in os.listdir(base_directory + folder):
+            print(file)
+            if file.endswith(".jpg") or file.endswith("jpeg") or file.endswith(".png") or file.endswith(".gif") or file.endswith(".webp") or file.endswith(".webm"):
+                f.write(
                     image_embed(file[:-4], folder, file)
                 )
-        readme.write("</details>\n")
-    readme.write(post)
+        f.write("</details>\n")
+    f.write(post)
