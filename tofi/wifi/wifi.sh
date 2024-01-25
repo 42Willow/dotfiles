572b71b2131743136e95c29a1fa62293ad70cf0b
42Willow <70784752+42Willow@users.noreply.github.com>
2024-01-25 15:56:02 +1100

    fix wifi script

--- tofi/wifi/wifi.sh
+++ tofi/wifi/wifi.sh
@@ -2,7 +2,7 @@
 
 nmcli device wifi rescan
 
-ssid=$(nmcli -f SSID dev wifi | awk 'NR>1 {sub(/[ \t]+$/, ""); print}' | grep -v -- | tofi)
+ssid=$(nmcli -f SSID dev wifi | awk 'NR>1 {sub(/[ \t]+$/, ""); print}' | grep -v -e "--" | tofi)
 output=$(nmcli device wifi connect "$ssid" 2>&1)
 if [ $? -eq 0 ]; then
     dunstify "󰖩  WiFi Connected" "SSID: $ssid"
