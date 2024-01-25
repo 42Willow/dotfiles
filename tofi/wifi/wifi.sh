1c31dd8815d7fe4dc3ae98a78dd3264370e3a8a5
42Willow <70784752+42Willow@users.noreply.github.com>
2024-01-25 15:57:29 +1100

    adjust prompt text

--- tofi/wifi/wifi.sh
+++ tofi/wifi/wifi.sh
@@ -2,7 +2,7 @@
 
 nmcli device wifi rescan
 
-ssid=$(nmcli -f SSID dev wifi | awk 'NR>1 {sub(/[ \t]+$/, ""); print}' | grep -v -e "--" | tofi)
+ssid=$(nmcli -f SSID dev wifi | awk 'NR>1 {sub(/[ \t]+$/, ""); print}' | grep -v -e "--" | tofi --prompt-text "connect: ")
 output=$(nmcli device wifi connect "$ssid" 2>&1)
 if [ $? -eq 0 ]; then
     dunstify "󰖩  WiFi Connected" "SSID: $ssid"
