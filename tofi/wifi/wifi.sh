46894c3db22b77d50ccb6638bc4d7d5dd02b5256
42Willow <70784752+42Willow@users.noreply.github.com>
2024-01-25 15:55:48 +1100

    move scripts

---
+++ tofi/wifi/wifi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+nmcli device wifi rescan
+
+ssid=$(nmcli -f SSID dev wifi | awk 'NR>1 {sub(/[ \t]+$/, ""); print}' | grep -v -- | tofi)
+output=$(nmcli device wifi connect "$ssid" 2>&1)
+if [ $? -eq 0 ]; then
+    dunstify "󰖩  WiFi Connected" "SSID: $ssid"
+else
+    dunstify -u 2 "󱚵  WiFi Connection Failed" "$output"
+fi
