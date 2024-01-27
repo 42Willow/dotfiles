132f9aaa9a8d057b53b97f9783991973850aca93
42Willow <70784752+42Willow@users.noreply.github.com>
2024-01-27 17:30:28 +1100

    manually define font for tofi

--- tofi/power_menu.sh
+++ tofi/power_menu.sh
@@ -4,7 +4,7 @@
 options="Shutdown\nReboot\nSuspend\nLog Out\nDisplays Off"
 
 # Display the power menu and capture the selection
-selected_option=$(echo -e "$options" | tofi --prompt-text "Power Menu: ")
+selected_option=$(echo -e "$options" | tofi --prompt-text "Power Menu: " --font /usr/share/fonts/OTF/MonaspiceNeNerdFont-Regular.otf)
 
 # Process the selected option
 case "$selected_option" in
--- tofi/wifi/wifi.sh
+++ tofi/wifi/wifi.sh
@@ -2,7 +2,7 @@
 
 nmcli device wifi rescan
 
-ssid=$(nmcli -f SSID dev wifi | awk 'NR>1 {sub(/[ \t]+$/, ""); print}' | grep -v -e "--" | tofi --prompt-text "connect: ")
+ssid=$(nmcli -f SSID dev wifi | awk 'NR>1 {sub(/[ \t]+$/, ""); print}' | grep -v -e "--" | tofi --prompt-text "connect: " --font /usr/share/fonts/OTF/MonaspiceNeNerdFont-Regular.otf)
 output=$(nmcli device wifi connect "$ssid" 2>&1)
 if [ $? -eq 0 ]; then
     dunstify "󰖩  WiFi Connected" "SSID: $ssid"
