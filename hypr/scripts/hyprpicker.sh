90c6f8c131ab12982a5deb22828456e6c9eea6df
42Willow <70784752+42Willow@users.noreply.github.com>
2024-02-04 18:02:01 +1100

    minor impovements

--- hypr/scripts/hyprpicker.sh
+++ hypr/scripts/hyprpicker.sh
@@ -2,4 +2,6 @@
 
 # run hyprpicker and then dunstify the output
 output=$(hyprpicker --autocopy)
-dunstify "Picked $output" "<span foreground='$output' size='xx-large'></span>"
+if [ -n "$output" ]; then
+    dunstify "Picked $output" "<span foreground='$output' size='xx-large'></span>"
+fi
