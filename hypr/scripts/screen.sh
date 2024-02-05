918d3c4069ea7c1fe08847d1839535d52211de88
42Willow <70784752+42Willow@users.noreply.github.com>
2024-02-05 20:17:06 +1100

    feat: screen rec
    screen recording is finally finished! (minus audio recording)

--- hypr/scripts/screen.sh
+++ hypr/scripts/screen.sh
@@ -3,22 +3,38 @@
 #!/bin/bash
 
 if ! pgrep -x "wl-screenrec" > /dev/null; then # if wl-screenrec is running
+    status_file="/tmp/screen_recording_status"
     region="$(slurp -d -c f5bde6)"
     recordings_dir="$HOME/Videos/Recordings"  # Set the desired directory
     filename="$recordings_dir/screen_recording_$(date +"%d-%m-%Y_%H-%M-%S").mp4"
+    start_time=$(date +"%s")
+
     if [ ! -z "$region" ]; then
-        pkill -USR1 -x screend.sh
+        echo "Recording " > "$status_file"
         mkdir -p "$recordings_dir"
         if [ "$1" == "audio" ]; then
             dunstify "Screen recording" " Audio enabled"
-            wl-screenrec -g "$region" -f "$filename" --audio --low-power=off &
+            wl-screenrec -g "$region" -f "$filename" --audio --audio-backend pactl info | awk '/Default Sink/ {print $3}' --low-power=off &
         else
             dunstify "Screen recording" " Audio disabled"
-            wl-screenrec -g "$region" -f "mute_$filename" --low-power=off &
+            wl-screenrec -g "$region" -f "$filename" --low-power=off &
         fi
-        wait $! # Wait for the last background process to finish
-        pkill -USR2 -x screend.sh
-        dunstify "Screen recording saved"
+        # wait $! # Wait for the last background process to finish
+       while pgrep -x "wl-screenrec" > /dev/null; do
+            current_time=$(date +"%s")
+            elapsed_time=$((current_time - start_time))
+            formatted_time=$(date -u -d @"$elapsed_time" +"%T")
+            echo -ne "  $formatted_time" > "$status_file"
+            sleep 0.5
+        done
+        echo "Screen recording saved"
+        rm "$status_file"
+
+        nautilus_response=$(dunstify -t 0 -A yes,nautilus "  Screen recording saved" "Click to view in nautilus")
+        
+        if [ "$nautilus_response" == "yes" ]; then
+            nautilus "$filename"
+        fi
     fi
 else
     dunstify "Screen recording cancelled" "A screen recording is already running"
--- hypr/scripts/screend.sh
+++ hypr/scripts/screend.sh
@@ -1,38 +1,12 @@
-# Slightly modified version of https://github.com/quantum5/wayland-recorder/blob/master/record-screend
-
 #!/bin/bash
 
-recordings=0
+status_file="/tmp/screen_recording_status"
 
-update() {
-    # echo "recordings: $recordings"
-    if [ "$recordings" -gt 0 ]; then
-        echo "  Recording"
+while true; do
+    if [ -e "$status_file" ]; then
+        echo "$(cat "$status_file")"
     else
-        echo 
+        echo  
     fi
-}
-
-begin_record() {
-    # echo "begin_record called"
-    recordings=$((recordings + 1))
-    update
-}
-
-end_record() {
-    # echo "end_record called"
-    recordings=$((recordings - 1))
-    update
-}
-
-exec sleep infinity &
-
-pid="$!"
-# echo "Script PID: $pid"
-trap begin_record SIGUSR1
-trap end_record SIGUSR2
-trap "kill $pid" EXIT
-
-while :; do
-    wait "$pid"
+    sleep 1
 done
